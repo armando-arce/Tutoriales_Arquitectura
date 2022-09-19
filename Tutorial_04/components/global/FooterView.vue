930df05f62bb37e77d946d157abf3932b9b099c7
armando-arce <71146244+armando-arce@users.noreply.github.com>
2022-09-19 14:44:14 -0600

    Delete Tutorial_04/components/global directory

--- Tutorial_04/components/global/FooterView.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="row"  style="margin-top: 15px">
-    <button disabled="disabled" class="twelve columns button-primary">
-      Copyright (c) 2021 - Armando Arce-Orozco
-    </button>
-  </div>
-</template>
